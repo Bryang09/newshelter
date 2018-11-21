c31dfcdf8adcd1f5b513e4a49ba412db08ae9929
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-11-21 09:14:18 -0600

    Updates

---
+++ precache-manifest.81d478cd35923e71914ad2f50c1c6fec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c11033d2238d2e3a9aed",
+    "url": "/newshelter/static/js/runtime~main.c11033d2.js"
+  },
+  {
+    "revision": "8c2abba62c6d07e12f5a",
+    "url": "/newshelter/static/js/main.8c2abba6.chunk.js"
+  },
+  {
+    "revision": "22e40587495d18cc3f2c",
+    "url": "/newshelter/static/js/1.22e40587.chunk.js"
+  },
+  {
+    "revision": "8c2abba62c6d07e12f5a",
+    "url": "/newshelter/static/css/main.79e9a1b8.chunk.css"
+  },
+  {
+    "revision": "981854d71fd48c38e02e2daec03e45bc",
+    "url": "/newshelter/index.html"
+  }
+];
